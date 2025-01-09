48a12ee5f486b938e5b084c17e8dc0f50b6bb521
Olatunji S <teejayx@outlook.com>
2025-01-09 19:01:15 +0000

    refactoring

---
+++ internal/transport/http.go
@@ -0,0 +1,60 @@
+package transport
+
+import (
+	"encoding/json"
+	"log"
+	"my-first-api/internal/todo"
+	"net/http"
+)
+
+type TodoItem struct{
+	Item string `json:"item"`
+} 
+
+type Server struct {
+	mux *http.ServeMux
+}
+
+func NewServer(todoSvc *todo.Service) *Server {
+
+     mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		response, err := json.Marshal(todoSvc.GetTodos())
+		if err != nil {
+			http.Error(w, "Error converting todos to JSON", http.StatusInternalServerError)
+			return
+		}
+		_, err = w.Write(response)
+		if err != nil {
+			log.Printf("Error writing response: %v", err)
+		}
+	})
+
+	mux.HandleFunc("POST /todo", func(w http.ResponseWriter, r *http.Request) {	
+		var t TodoItem
+		err := json.NewDecoder(r.Body).Decode(&t)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		todoSvc.AddTodo(t.Item)
+		w.WriteHeader(http.StatusCreated)
+		return
+        
+	})
+	return &Server{mux: mux}
+
+
+}
+
+func (s *Server) Serve() error {
+	return http.ListenAndServe(":8080", s.mux)
+}
--- main.go
+++ main.go
@@ -1,59 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"my-first-api/internal/todo"
-	"net/http"
+	"my-first-api/internal/transport"
+
 )
 
-type TodoItem struct{
-	Item string `json:"item"`
-} 
 
 
 func main() {
 	svc := todo.NewService()
-	mux := http.NewServeMux()
+	server := transport.NewServer(svc)
  
-	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		response, err := json.Marshal(svc.GetTodos())
-		if err != nil {
-			http.Error(w, "Error converting todos to JSON", http.StatusInternalServerError)
-			return
-		}
-		_, err = w.Write(response)
-		if err != nil {
-			log.Printf("Error writing response: %v", err)
-		}
-	})
-
-	mux.HandleFunc("POST /todo", func(w http.ResponseWriter, r *http.Request) {	
-		var t TodoItem
-		err := json.NewDecoder(r.Body).Decode(&t)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		svc.AddTodo(t.Item)
-		w.WriteHeader(http.StatusCreated)
-		return
-        
-	})
-
-
+	if err := server.Serve(); err != nil {
+		log.Fatal(err)
+	}
 
 
 
 	log.Println("Server starting on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+	
 }
